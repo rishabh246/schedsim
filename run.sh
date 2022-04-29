6345cab9e50888db03666d7e927736dc2aed7f45
rishabh246 <rishabh246@gmail.com>
2022-04-29 11:33:08 +0200

    Remove comments, add build

--- run.sh
+++ run.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+go build 
+
 # Commands to run all three configurations for the MB[99.5-0.5] workload
  
-# python3 ./scripts/run_many.py run --topo=0 --mu=0.333611 --gen_type=3 --proc_type=0 --num_cores=16 
-# mv out.txt zygos.txt
-# python3 ./scripts/run_many.py csv zygos.txt zygosKeeper0.csv Keeper0 
-# python3 ./scripts/run_many.py csv zygos.txt zygosKeeper1.csv Keeper1 
+python3 ./scripts/run_many.py run --topo=0 --mu=0.333611 --gen_type=3 --proc_type=0 --num_cores=16 
+mv out.txt zygos.txt
+python3 ./scripts/run_many.py csv zygos.txt zygosKeeper0.csv Keeper0 
+python3 ./scripts/run_many.py csv zygos.txt zygosKeeper1.csv Keeper1 
 
 python3 ./scripts/run_many.py run --topo=0 --mu=0.333611 --gen_type=3 --proc_type=2 --num_cores=16 --quantum=5 --ctxCost=1
 mv out.txt shinjuku.txt
 python3 ./scripts/run_many.py csv shinjuku.txt shinjukuKeeper0.csv Keeper0 
 python3 ./scripts/run_many.py csv shinjuku.txt shinjukuKeeper1.csv Keeper1 
 
-# python3 ./scripts/run_many.py run --topo=0 --mu=0.333611 --gen_type=3 --proc_type=2 --num_cores=16 --quantum=5 --ctxCost=0.1
-# mv out.txt concord.txt
-# python3 ./scripts/run_many.py csv concord.txt concordKeeper0.csv Keeper0 
-# python3 ./scripts/run_many.py csv concord.txt concordKeeper1.csv Keeper1 
+python3 ./scripts/run_many.py run --topo=0 --mu=0.333611 --gen_type=3 --proc_type=2 --num_cores=16 --quantum=5 --ctxCost=0.1
+mv out.txt concord.txt
+python3 ./scripts/run_many.py csv concord.txt concordKeeper0.csv Keeper0 
+python3 ./scripts/run_many.py csv concord.txt concordKeeper1.csv Keeper1 
